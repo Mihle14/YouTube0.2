a157eb38081b1b656ba650dbe94971836755dad3
Mihle14 <mihlentsaluba14@gmail.com>
2025-11-20 21:43:52 +0200

    video

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :image)
+      params.require(:post).permit(:title, :description, :image, :video)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user,optional: true
     has_one_attached :image
+    has_one_attached :video
     after_initialize { self.views ||= 0 }
     validates :title, presence: true
 end
